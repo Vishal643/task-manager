c7f7b09caa664564c71d3c90f23f72626fbfba66
Vishal643 <vishalkumar199812@gmail.com>
2024-02-10 11:05:09 +0530

    fix: route name updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const baseRoute = '';
+const baseRoute = '/api/v1/';
 
 // health check route
 app.get(`${baseRoute}/health`, (req, res) => {
